bdccd70b49404a50831a7692ca509ea6c5b40a95
Christina <cjk.uxui@gmail.com>
2021-09-22 20:41:32 -0400

    19.2

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,43 @@
 // Create a variable below
 
+
+var myfirstvariable="This is my first variable";
+
+var Christina="My name is Christina";
+
+
+
 // Console.log() your variable here.
+
+console.log(myfirstvariable);
+
+console.log(Christina);
+
+var redhat=5;
+var bluehat=9;
+
+
+var total='Your total is' + (redhat + bluehat);
+
+alert(total);
+
+function addNumbers() {
+var two=2;
+var three=3;
+var five= two+three;
+
+console.log(five);
+}
+
+
+function addNumbers() {
+    var two=' Janisa';
+    var three=' Michaela';
+    var five= 'Our teachers are' + (two+three);
+    
+    console.log(five);
+    }
+
+
+
+addNumbers();
